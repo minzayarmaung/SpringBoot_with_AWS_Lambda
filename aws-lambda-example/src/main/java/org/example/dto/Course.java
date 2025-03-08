c88cfb41a22ede1cd5d7fe59c8a9a132bf6af6e6
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-03-08 15:21:42 +0630

    add latest code

--- aws-lambda-example/src/main/java/org/example/dto/Course.java
+++ aws-lambda-example/src/main/java/org/example/dto/Course.java
@@ -1,5 +1,6 @@
 package org.example.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Course {
 
     private int id;
     private String name;
     private double price;
+
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 }
